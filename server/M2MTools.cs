7a8ebeb6b1470483b5afc51337ce58a46f2cf03e
MdeJ <none@none.com>
2010-07-30 12:04:52 +0000

    Imported m2m4ria version 20100730

---
+++ server/M2MTools.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.Objects;
+using System.Data.Objects.DataClasses;
+
+
+namespace EF.M2M
+{
+    public static class M2MTools
+    {
+
+        /// <summary>
+        /// http://blogs.msdn.com/b/alexj/archive/2009/06/19/tip-26-how-to-avoid-database-queries-using-stub-entities.aspx
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="ctx"></param>
+        /// <param name="qualifiedEntitySetName"></param>
+        /// <param name="stubEntity"></param>
+        /// <returns></returns>
+        public static T GetEntityByKey<T>(ObjectContext ctx, string qualifiedEntitySetName, T stubEntity) where T : EntityObject
+        {
+            ObjectStateEntry state;
+            EntityKey key = ctx.CreateEntityKey(qualifiedEntitySetName, stubEntity);
+            if (ctx.ObjectStateManager.TryGetObjectStateEntry(key, out state) == false)
+            {
+                ctx.AttachTo(qualifiedEntitySetName, stubEntity);
+                return stubEntity;
+            }
+            else
+            {
+                return (T)state.Entity;
+            }
+        }
+    }
+}
