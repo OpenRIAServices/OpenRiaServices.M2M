b2928279c4dc4ee3099497acf3004d081621839d
Daniel Svensson <daniel.svensson@hotmail.se>
2019-05-22 17:07:17 +0200

    Prepare for new preview release (#7)
    * use new version of FluentMetadata
    
    * add and use snk file

--- build_nuget.cmd
+++
@@ -1,7 +0,0 @@
-
-@set OPTIONS=-build -verbosity normal -Prop Configuration=Release -OutputDirectory nupkgs -IncludeReferencedProjects
-
-@if not exist nupkgs (mkdir nupkgs)
-@NuGet.exe pack OpenRiaServices.M2M.LinkTable\OpenRiaServices.M2M.LinkTable.csproj %OPTIONS%
-@NuGet.exe pack OpenRiaServices.M2M.Silverlight\OpenRiaServices.M2M.Silverlight.csproj %OPTIONS%
-@NuGet.exe pack OpenRiaServices.M2M\OpenRiaServices.M2M.csproj %OPTIONS%
